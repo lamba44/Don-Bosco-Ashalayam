1cc347fa79b9bb67ada7bf08ddb954c219e21bbc
Saksham Lamba <sakshamlambamonu@gmail.com>
2025-01-01 21:25:12 +0530

    fix: CI/CD fixes, Sample deployment

--- frontend/src/Components/Hero.jsx
+++ frontend/src/Components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
     return (
         <div className="hero container">
             <div className="hero-text">
-                <h1>Don Bosco Ashalayam</h1>
+                <h1>Don Bosco Ashalayam (Test Website)</h1>
                 <p>A home for the young at risk</p>
                 <button className="btn">Get Involved</button>
             </div>
--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-    Navigate,
-} from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App";
 
 const AppRoutes = () => {

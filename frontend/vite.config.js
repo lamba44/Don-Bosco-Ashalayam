10adb5eb2f0699710a86001c45d371b820b5feb4
Saksham Lamba <sakshamlambamonu@gmail.com>
2025-01-20 00:27:05 +0530

    fix: vite.config.js change for live CI/CD

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "/", //Change to "/" for deployment AND/OR development
+    base: "/Don-Bosco-Ashalayam/", //Change to "/" for deployment AND/OR development
 });

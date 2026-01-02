b10e5b679f292df45c24e6357b1fe2b0f1c82521
Saksham Lamba <sakshamlambamonu@gmail.com>
2026-01-03 00:39:55 +0530

    update: Analytics and Speed Insights added for Vercel deployment

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import AppRoutes from "./Routes.jsx";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
         <AppRoutes />
+        <Analytics />
+        <SpeedInsights />
     </StrictMode>
 );
